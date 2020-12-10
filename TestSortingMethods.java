61a5d39d98deec95e9d644f02e9068216bf3e605
Victor Valdez <victor334_@hotmail.com>
2020-12-10 10:43:38 -0400

    testBubbleUpAnotherArray test added

--- TestSortingMethods.java
+++ TestSortingMethods.java
@@ -10,12 +10,24 @@ public class TestSortingMethods {
 
     @Test
     public void testBubbleUp(){ 
-        int[] array = new int[] {3, 5, 5, 2, 1};
+        int[] array = new int[] {3, 5, 5, 7, 2, 1};
         List<Integer> list = new ArrayList<Integer>();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(5);
+        list.add(7);
+        assertEquals(list, sorting.BubbleUp(array));
+    }
+
+    
+    @Test
+    public void testBubbleUpAnotherArray(){ 
+        int[] array = new int[] {5, 7, 1, 1};
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(5);
+        list.add(7);
         assertEquals(list, sorting.BubbleUp(array));
     }
 }
--- sorting.java
+++ sorting.java
@@ -1,15 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class sorting {
 
     public static void main(String[] args) {
-        int[] array = new int[] {5, 5, 7, 3, 2, 1};
-        BubbleUp(array);
+        
     }
 
-    public static int[] BubbleUp(int[] array){
-
+    public static List<Integer> BubbleUp(int[] array){
         int temp = 0;
+        List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1; j++) {
                 if(array[j] > array[j + 1]){
@@ -20,10 +21,19 @@ public class sorting {
             }
         }
 
-        for (int i : array) {
-            System.out.println(i);
+        for (int i = 0; i < array.length - 1; i++) {
+            result.add(array[i]);
         }
 
-        return array; 
+        for (int i = 0; i < result.size() - 1; i++) {
+            if (result.toArray()[i] == result.toArray()[i+1]){
+                result.remove(i);
+            }
+        }
+
+        for(int x: result){
+            System.out.println(x);
+        }
+        return result; 
     }
 }
