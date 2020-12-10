5286a14851b157553f468c8effabd3986b693fd9
Victor Valdez <victor334_@hotmail.com>
2020-12-10 10:43:57 -0400

    BubbleUp method refactored

--- sorting.java
+++ sorting.java
@@ -5,23 +5,24 @@ import java.util.List;
 public class sorting {
 
     public static void main(String[] args) {
-        
+        int[] arr = {5, 7, 1, 1};
+        BubbleUp(arr);
     }
 
     public static List<Integer> BubbleUp(int[] array){
-        int temp = 0;
         List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
                 if(array[j] > array[j + 1]){
-                    temp = array[j];
+                    int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
                 }
             }
         }
 
-        for (int i = 0; i < array.length - 1; i++) {
+
+        for (int i = 0; i < array.length; i++) {
             result.add(array[i]);
         }
 
@@ -30,10 +31,11 @@ public class sorting {
                 result.remove(i);
             }
         }
-
+        
         for(int x: result){
             System.out.println(x);
         }
+        
         return result; 
     }
 }
