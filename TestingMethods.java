53fe5b6a1456ad983aaf2f4ed095c7f136286b9f
Victor Valdez <victor334_@hotmail.com>
2020-12-11 19:59:56 -0400

    all changes saved

--- TestingMethods.java
+++ TestingMethods.java
@@ -32,10 +32,20 @@ public class TestingMethods {
     @Test
     public void testEliminateDuplicates(){
         int[] array = new int[] {33, 4, 4, 1};
+        int[] anotherArray = new int[] {33, 4, 4, 1, 3, 7, 9, 10};
         List<Integer> output= new ArrayList<Integer>();
         output.add(1);
         output.add(4);
         output.add(33);
+        List<Integer> anotherOutput= new ArrayList<Integer>();
+        anotherOutput.add(1);
+        anotherOutput.add(3);
+        anotherOutput.add(4);
+        anotherOutput.add(7);
+        anotherOutput.add(9);
+        anotherOutput.add(10);
+        anotherOutput.add(33);
         assertEquals(output, Sorting.EliminateDuplicates(Sorting.BubbleUp(array)));
+        assertEquals(anotherOutput, Sorting.EliminateDuplicates(Sorting.BubbleUp(anotherArray)));
     }
 }
