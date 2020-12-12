94e348ac7bf573132410a9a826c203555d8c3989
Victor Valdez <victor334_@hotmail.com>
2020-12-12 18:32:07 -0400

    3rd initial commit: test & code files created

---
+++ TestMethods.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * TestingMethods
+ */
+public class TestingMethods {
+
+    @Test
+    public void testBubbleUp(){ 
+        assertEquals(expect, actual);
+    }
